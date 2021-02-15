b698cf3a129a158c0006f0137ad65334ee6bdfaa
Ian Cossentino <icossentino@gmail.com>
2021-02-15 13:33:14 -0500

    commit tree e6193347c8ff83fd3787a5cf758dfbdc3079c898

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+
+function writeCards(namesArray, adj) {
+  let thanksArray = [];
+  for (let i = 0; i < namesArray.length; i++) {
+    thanksArray.push(`Thank you, ${namesArray[i]}, for the wonderful ${adj} gift!`)
+  }
+  return thanksArray
+}
+
+
+function countDown(amt) {
+  let i = amt
+  while (i >= 0) {
+    console.log(i)
+    i -= 1
+  }
+}
